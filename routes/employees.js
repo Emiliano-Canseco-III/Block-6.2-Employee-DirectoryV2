e622f8a17c8a7851c533b5aff6b330ce6e096d6b
Milo Canseco <canseco3rd@gmail.com>
2025-12-17 18:28:19 -0700

    Added all employee routes and added missig req.body handler at the beginning of router.post

--- routes/employees.js
+++ routes/employees.js
@@ -1,7 +1,10 @@
 import express from "express";
 import employees from "../db/employees.js";
-
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json(employees);
+});
 
 router.get("/random", (req, res) => {
   const randomIndex = Math.floor(Math.random() * employees.length);
@@ -10,28 +13,33 @@ router.get("/random", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  const employee = employees.find((e) => e.id === +id);
+  const employee = employees.find((e) => e.id === Number(id));
 
   if (!employee) {
     return res.status(404).send("Employee not found.");
   }
 
-  res.send(employees);
+  res.json(employee);
 });
 
 router.post("/", (req, res) => {
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return res.status(400).send("Request body or name is missing.");
+  }
+
   const { name } = req.body;
 
   if (!name || typeof name !== "string") {
-    return res.status(400).send("Name is required and must be a string.");
+    return res.status(400).send("Name is required.");
   }
 
   const newEmployee = {
     id: employees.length + 1,
-    name: name,
+    name,
   };
-
   employees.push(newEmployee);
+
+  res.status(201).json(newEmployee);
 });
 
 export default router;
