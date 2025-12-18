3798bee36cffcf02f9090b555f8e6d269df30c78
Milo Canseco <canseco3rd@gmail.com>
2025-12-17 18:27:06 -0700

    Removed all employee routes and moved it to the employee.js file in routes folder.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from "express";
-import employees from "#db/employees";
-import employeeRouter from "./routes/employees";
+import employeeRouter from "./routes/employees.js";
 
 const app = express();
 
@@ -12,34 +11,9 @@ app.get("/", (req, res) => {
 
 app.use("/employees", employeeRouter);
 
-app.get("/employees", (req, res) => {
-  res.send(employees);
-});
-
-// Note: this middleware has to come first! Otherwise, Express will treat
-// "random" as the argument to the `id` parameter of /employees/:id.
-app.get("/employees/random", (req, res) => {
-  const randomIndex = Math.floor(Math.random() * employees.length);
-  res.send(employees[randomIndex]);
-});
-
-app.get("/employees/:id", (req, res) => {
-  const { id } = req.params;
-
-  // req.params are always strings, so we need to convert `id` into a number
-  // before we can use it to find the employee
-  const employee = employees.find((e) => e.id === +id);
-
-  if (!employee) {
-    return res.status(404).send("Employee not found");
-  }
-
-  res.send(employee);
-});
-
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something went wrong!");
+  res.status(500).send("Internal server Error");
 });
 
 export default app;
